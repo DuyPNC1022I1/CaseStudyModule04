9be62747dc70f7dad4f4b2d419039b09aee2ca1a
trmanhthang <thangmt868@gmail.com>
2023-03-15 15:58:37 +0700

    git lai

--- src/main/java/media/web/mediaweb/controller/AdminController.java
+++ src/main/java/media/web/mediaweb/controller/AdminController.java
@@ -1,14 +1,11 @@
 package media.web.mediaweb.controller;
 
-import media.web.mediaweb.model.Song;
 import media.web.mediaweb.service.AlbumService;
 import media.web.mediaweb.service.SingerService;
 import media.web.mediaweb.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +30,6 @@ public class AdminController {
         model.addAttribute("songs", songService.findAll(pageable));
         model.addAttribute("singer", albumService.findAll());
         model.addAttribute("album", singerService.findAll());
-        return "templates/view/soundwave/admin";
+        return "static/image/soundwave/admin";
     }
 }
