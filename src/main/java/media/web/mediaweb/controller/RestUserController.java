9e59cd18067841b802e1103563844434618629d0
trmanhthang <bigzeen861998@gmail.com>
2023-03-15 09:11:41 +0700

    mo quyen

--- src/main/java/media/web/mediaweb/controller/RestUserController.java
+++ src/main/java/media/web/mediaweb/controller/RestUserController.java
@@ -1,10 +1,15 @@
 package media.web.mediaweb.controller;
 
 
+import media.web.mediaweb.model.Song;
 import media.web.mediaweb.service.impl.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -13,4 +18,10 @@ import org.springframework.web.bind.annotation.*;
 public class RestUserController {
     @Autowired
     private SongService songService;
+
+    @GetMapping
+    public ResponseEntity<List<Song>> findAll() {
+        List<Song> songList = songService.findAll();
+        return new ResponseEntity<>(songList, HttpStatus.OK);
+    }
 }
