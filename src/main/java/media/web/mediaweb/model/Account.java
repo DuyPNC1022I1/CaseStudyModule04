edc2bdaff5da6da6df7a20e4bc3eaec6aee69944
trmanhthang <bigzeen861998@gmail.com>
2023-03-15 08:38:40 +0700

    update login

--- src/main/java/media/web/mediaweb/model/Account.java
+++ src/main/java/media/web/mediaweb/model/Account.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -20,24 +24,25 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-    @Column(unique = true)
+//    @Column(unique = true)
     private String username; //Cần validate unique
-    @Size(min = 6, max = 8)
+//    @Size(min = 6, max = 8)
     private String password; //Cần validate 6-8 ký tự
-    @Size(min = 10, max = 12)
+//    @Size(min = 10, max = 12)
     private String phone; //Cần validate 12 ky tu
-    @Email
+//    @Email
     private String email; //Cần validate @
+
     private String address;
 
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Role> roles; // Kiểm tra lại
 
-//    public List<GrantedAuthority> getAuthorities() {
-//        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-//        for (Role role : roles) {
-//            authorities.add(new SimpleGrantedAuthority(role.getName()));
-//        }
-//        return authorities;
-//    }
+    public List<GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
 }
